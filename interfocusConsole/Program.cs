1f6119b3ab98aecc72c338a10d2f55e6519b3ce8
nxll0 <126991927+AllanShima@users.noreply.github.com>
2024-04-27 12:54:11 -0300

    Update Program.cs

--- interfocusConsole/Program.cs
+++ interfocusConsole/Program.cs
@@ -1,20 +1,14 @@
-﻿/*bool b = true;
-double d = 1.25;
+// See https://aka.ms/new-console-template for more information
 
-var lista = new List<string>();
-lista.Add("Item 1");
-lista.Add("1235");
+using Interfocus;
+using InterfocusConsole;
+using System.Linq.Expressions;
 
-var lista2 = lista;
+Console.WriteLine("Hello, World!");
 
-lista2.Add("grfds");
-
-Console.WriteLine($"Lista: {lista.Count} Lista 2: {lista2.Count}");
-
-
-var x = 5;
-var y = x;
-var soma = x + y;
+int x11 = 5;
+var y = 5;
+var soma = x11 + y;
 
 var maiorQueDez = soma > 10;
 var impar = soma % 2 == 1;
@@ -24,10 +18,12 @@ var comb = maiorQueDez && impar;
 if (comb)
 {
     Console.WriteLine("é maior que dez e impar");
-} else if (x == 5)
+}
+else if (5 == 5)
 {
     Console.WriteLine("ele é 5");
-} else
+}
+else
 {
     Console.WriteLine("grz");
 }
@@ -42,17 +38,56 @@ for (int i = 0; i <= 10; i++)
 var data = new DateTime(2024, 4, 20, 10, 44, 12);
 Console.WriteLine("{0}", data);
 
-*/
+// CHATBOT ---------------------------------------
 
+//Console.Clear();
+//
+//
+//
+//var objeto = new Metodos();
+//
+// var a1 = new Aluno
+//{
+//    Nome = "Allan",
+//    DataNascimento = new DateTime(2000, 1, 1),
+//    Codigo = 1001
+//};
+//
+////chamando o set
+//a1.Email = "allanshinhamabelo@gmail.com";
+//
+////chamando o get
+//Console.WriteLine(a1.Email);
+//
+//var a2 = new Aluno();
+//
+//Console.WriteLine("Aluno 1: {0}", a1.Nome);
+//Console.WriteLine("Aluno 2: {0}", a2.Nome);
+//
+//Aluno a3 = null;
+//
+//var b1 = new Bolsista
+//{
+//    Nome = "Bolsista 1",
+//    Codigo = 1002,
+//    Desconto = 0.5
+//};
 
-// CHATBOT
-
-using Interfocus;
+//Console.WriteLine("Aluno 3: {0}", a3?.Nome);
 
 Console.Clear();
 
+var alunos = new List<Aluno>();
 var lista = new List<string>();
 var objeto = new Metodos();
+
+string Input(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+int codigo = 1000;
 
 while (true)
 {
@@ -61,13 +96,16 @@ while (true)
     Console.WriteLine("2 - Adicionar item lista");
     Console.WriteLine("3 - Printar lista");
     Console.WriteLine("4 - Buscar na lista");
-    Console.WriteLine("5 - Remover um item da lista");
+    Console.WriteLine("5 - Cadastrar aluno");
+    Console.WriteLine("6 - Listar alunos");
+    Console.WriteLine("7 - Buscar alunos");
+    Console.WriteLine("8 - Excluir aluno");
     Console.WriteLine("0 - Sair");
     var inputValido = int.TryParse(Console.ReadLine(), out int opcao);
 
     if (!inputValido)
     {
-        Console.WriteLine("Input inválido!");
+        Console.WriteLine("Input inválido!\n");
         Console.ReadKey();
         continue;
     }
@@ -103,12 +141,71 @@ while (true)
             objeto.PrintLista(resultado);
             break;
         case 5:
-            Console.WriteLine("Digite o indice do item: ");
-            var index = int.Parse(Console.ReadLine());
-            var removido = lista[index - 1];
-            Console.WriteLine("Removendo: {0}", removido);
+            var resposta = Input("Aluno bolsista?");
 
-            lista.RemoveAt(index - 1);
+            if (resposta == "S")
+            {
+                var novoAluno = new Bolsista
+                {
+                    Nome = Input("Digite o nome do aluno: "),
+                    Email = Input("Digite o email do aluno: "),
+                    DataNascimento = DateTime.Parse(Input("Digite as data de nascimento: ")),
+                    Desconto = double.Parse(Input("Porcentagem de desconto: ")),
+                    Codigo = codigo++
+                };
+                alunos.Add(novoAluno);
+            }
+            else
+            {
+                var novoAluno = new Aluno
+                {
+                    Nome = Input("Digite o nome do aluno: "),
+                    Email = Input("Digite o email do aluno: "),
+                    DataNascimento = DateTime.Parse(Input("Digite as data de nascimento: ")),
+                    Codigo = codigo++
+                };
+                AlunoService.CriarAluno(novoAluno);
+            }
             break;
+        case 6:
+            foreach (var aluno in AlunoService.Listar())
+            {
+                aluno.PrintDados();
+            }
+            break;
+        case 7:
+            var buscaAluno = Input("Digite a busca: ");
+            foreach (var aluno in AlunoService.Listar(buscaAluno))
+            {
+                aluno.PrintDados();
+            }
+            break;
+        case 8:
+            try
+            {
+                var codigoAluno = int.Parse(Input("Digite o codigo do aluno: "));
+                var removido = AlunoService.Remover(codigoAluno);
+                if (removido != null)
+                {
+                    removido.PrintDados();
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Digite um valor válido!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Deu ruim {0}", e.Message);
+            }
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break; 
+    }
+    Console.ReadKey();
+    if (opcao == 1)
+    {
+        
     }
 }
